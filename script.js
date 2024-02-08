a0d962eb47194bc475c535d4089c1a4f5aed2d32
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-08 13:09:58 -0600

    functions added, search() and searchHandler() with shorthand syntax

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
-
-	// TODO
-
-	return results;
+	return fruit.filter(fruit => fruit.toLowerCase().includes(str.toLowerCase()));
 }
 
 function searchHandler(e) {
-	// TODO
+	let inputVal = e.target.value;
+	inputVal.length > 0 ? showSuggestions(search(inputVal), inputVal) : suggestions.innerHTML = "";
 }
 
 function showSuggestions(results, inputVal) {
-
-	// TODO
+	suggestions.innerHTML = "";
+	results.forEach(result => {
+		let li = document.createElement('li');
+        li.textContent = result;
+		suggestions.appendChild(li);
+	});
 }
 
 function useSuggestion(e) {
-	// TODO
+	if(e.target.tagName === 'LI') {
+		input.value = e.target.textContent;
+		suggestions.innerHTML = "";
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
